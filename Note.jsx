6cc8a9a108cb1b6408f7c7c302fd4bb9eb1aba2d
GayuSri0506 <85435605+GayuSri0506@users.noreply.github.com>
2021-06-06 15:17:49 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Famous Books</h1>
+      <p>Five Books:</p>
+      <ul>
+        <li>Granny o coronnor</li>
+        <li>king lear</li>
+        <li>A mid summer nights dreams</li>
+        <li>Terry Eagleton</li>
+        <li>Ian Doescher</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Note;
